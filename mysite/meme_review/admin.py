6a416fdbd88a74efaf2b5b6b03238152b138ae91
Karl Rezansoff <karlrez123@gmail.com>
2020-04-19 15:52:20 -0400

    first edit

--- mysite/meme_review/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import MemeUpload
-#Comment
-# Register your models here.
-admin.site.register(MemeUpload)
-#admin.site.register(Comment)
--- mysite/meme_review/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MemePageConfig(AppConfig):
-    name = 'meme_page'
